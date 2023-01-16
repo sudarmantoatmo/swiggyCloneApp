69818b186ce055e9c6041032c3716eb529def95a
sudarmanto <sudarmanto.atmo@gmail.com>
2023-01-16 22:14:12 +0700

    generate tabs home

--- src/app/pages/tabs/home/home-routing.module.ts
+++ src/app/pages/tabs/home/home-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TabsPage } from './tabs.page';
+import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    component: HomePage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class TabsPageRoutingModule {}
+export class HomePageRoutingModule {}
